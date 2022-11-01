6320bb102a16b166830549045f533b0bdaf51f1e
ZeroRequiem74 <zerorequiem2474@gmail.com>
2022-11-01 13:44:02 -0600

    more initialization

--- Ores.cs
+++ Ores.cs
@@ -1,39 +1,79 @@
+using Raylib_cs;
+using System.Numerics;
+
 abstract class Ores
 {
+    bool mined;
+    bool fell;
+    int objects;
+    
 
 }
 
 class Stones: Ores
 {
-
+    int points = -1;
 }
 
 class Gems: Ores
 {
-
+    int points = 1;
 }
 
 class Destructable
 {
+    public int Broken(bool mined, bool fell, int objects)
+    {
+        if(mined | fell)
+        {
+            objects -= 1;
+        }
+        return objects;
+    }
+}
 
+class Creation
+{
+    public int NumberOfOres(int objects)
+    {
+        if (objects < 30)
+        {
+            objects += 1;
+        }
+        return objects;
+    }
 }
 
 class Points
 {
+    public bool AddPoints(bool mined, bool fell)
+    {
+        bool add = false;
 
+        if(mined)
+        {
+            add = true;
+        }
+        else if(fell)
+        {
+            add = false;
+        }
+        else
+        {
+            add = false;
+        }
+
+        return add;
+    }
 }
 
 class Movement
 {
-
-}
-
-class Appearance
-{
-
+    int MovementSpeed = 10;
 }
 
 class Miner
 {
+    int PlayerMovement = 15;
 
 }
