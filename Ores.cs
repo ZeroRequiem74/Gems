65f5afbc33e5ce728c323906c4e9a88a7fb9d3b7
ZeroRequiem74 <zerorequiem2474@gmail.com>
2022-11-01 12:57:02 -0600

    initialized

---
+++ Ores.cs
@@ -0,0 +1,39 @@
+abstract class Ores
+{
+
+}
+
+class Stones: Ores
+{
+
+}
+
+class Gems: Ores
+{
+
+}
+
+class Destructable
+{
+
+}
+
+class Points
+{
+
+}
+
+class Movement
+{
+
+}
+
+class Appearance
+{
+
+}
+
+class Miner
+{
+
+}
